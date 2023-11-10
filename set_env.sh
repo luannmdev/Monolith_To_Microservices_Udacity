1d924f0b448f318b0dab5d8e788a5d674f0f3781
DungGLA <adung.hver23@gmail.com>
2023-11-10 23:15:30 +0700

    Init monolith to microservice project

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=luannm9
+export POSTGRES_PASSWORD=luannm9password
+export POSTGRES_HOST=database-udagram.cafil2tzsjjo.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=udagram_luannm9_db
+export AWS_BUCKET=user-uploaded-s3-661438571346
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=luannm9secret
+export URL=http://localhost:8100
