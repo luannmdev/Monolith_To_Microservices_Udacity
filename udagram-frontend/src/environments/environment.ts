d3b489bdafcee7bb15027804dd2a5eef4efd53c8
luannmdev <Vnproben@gmail.com>
2023-11-23 19:12:06 +0700

    Add screenshots

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a16166bc911584195adf907c77ff6027-1218156836.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
